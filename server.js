71f75ac2a6fbef9e03194c2780bdd25a7c19b4ab
DangerRanger <dangerousdevops@gmail.com>
2015-04-06 15:50:15 -0700

    local server thing

---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+var routes = require('./server/routes/index');
+var users = require('./server/routes/users');
+var choons = require('./server/routes/choons');
+
+// database configuration
+var config = require('./server/config/config.js');
+mongoose.connect(config.url); // set to localhost
+
+// Check if MongoDB is running
+mongoose.connection.on('error', function() {
+  console.log('Make sure MongoDB is running.');
+});
+
+var app = express();
+// Passport configuration
+require('./server/config/passport')(passport);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'server/views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/api/choons', choons);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 8080);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+
+module.exports = app;
