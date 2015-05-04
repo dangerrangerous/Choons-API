18b50a41fd49a82f78077b84cdf0b9cb9624b672
DangerRanger <dangerousdevops@gmail.com>
2015-05-04 15:47:39 -0700

    article controller read() and update()

--- controllers/articles.server.controller.js
+++ controllers/articles.server.controller.js
@@ -42,3 +42,35 @@ exports.list = function(req, res) {
     }
   });
 };
+// we love the mongoose populate() method
+exports.articleByID = function(req, res, next, id) {
+  Article.findById(id).populate('creator', 'firstName lastName fullName').exec(function(err, article) {
+    if(err) return next(err);
+    if(!article) return next (new Error('Failed to load article ' + id));
+
+    req.article = article;
+    next();
+  });
+};
+
+exports.read = function(req, res) {
+  res.json(req.article);
+};
+
+exports.update = function(req, res) {
+  var article = req.article;
+
+  article title = req.body.title;
+
+  article.content = req.body.content;
+
+  article.save(function(err) {
+    if (err) {
+      return res.status(400).send({
+      message: getErrorMessage(err)
+    });
+    } else {
+    res.json(article);
+    }
+  });
+};
