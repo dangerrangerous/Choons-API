50d71d32ac5bdcd53604bab9aa51f69c08420389
DangerRanger <dangerousdevops@gmail.com>
2015-05-04 14:28:14 -0700

    article controller simple error handling

--- controllers/articles.server.controller.js
+++ controllers/articles.server.controller.js
@@ -1,2 +1,14 @@
 var mongoose = require('mongoose'),
   Article = mongoose.model('Article');
+
+// extract FIRST error message as to not overwhelm users with error
+// messages so they can focus on one at a time
+var getErrorMessage = function(err) {
+  if (err.errors) {
+    for (var errName in err.errors) {
+      if (err.errors[errName].message) return err.errors[errName].message;
+    }
+      }  else {
+        return 'Uknown (possibly) server error. Sorry, Good luck mate';
+      }
+};
