394cda9939a28190b69aad9dc2d357b8d3fa7176
DangerRanger <dangerousdevops@gmail.com>
2015-05-04 14:18:16 -0700

    article controller is born

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -6,6 +6,8 @@ module.exports = function() {
 
   require('../app/models/user.server.model');
   require('../app/models/article.server.model');
-  
+
   return db;
 };
+
+// note to self, var db may or may not be needed in our server.js file
