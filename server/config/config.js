f7164b721f9b7b4ecc353a0c9340782b47ec43ba
DangerRanger <dangerousdevops@gmail.com>
2015-05-04 14:01:12 -0700

    setting up mongoose config

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 // Database URL
 module.exports = {
   // Uncomment to connect with MongoDB on Cloud
-  // thinking maybe MongoLabs or MongoHq
+  // still considering deployment options
   'url' : 'mongodb://localhost/choonsDB' // double check this correctness
 };
